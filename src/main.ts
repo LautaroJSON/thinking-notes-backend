3de7de62634724609478b7c9466901f48ff37020
Lautaro Fernandez <fernandez.n.lautaro@gmail.com>
2026-04-14 02:08:25 +0000

    feat: habilitar CORS con configuraciones específicas para el backend

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,17 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   console.log('¿Nest ve la URL?:', process.env.DATABASE_URL);
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:5173',
+      // Agrega tu URL de GitHub Pages aquí
+      'https://lautarojson.github.io',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
